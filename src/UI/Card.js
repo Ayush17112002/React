a952c9242056b38f1ec3da1d4dca456536e749c9
Ayush <aayushgupta023@gmail.com>
2022-09-11 20:41:38 +0530

    done

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,12 +1,11 @@
-//card is used to give some common styling 
-import './Card.css';
-function Card(props){
-    let classes = 'card ';
-    if(props.className !== undefined){
-        classes += props.className;
-    }
-    return(
-        <div className={classes}>{props.children}</div>
-    );
+//card is used to give some common styling
+import "./Card.css";
+function Card(props) {
+  let classes = "card ";
+  if (props.className !== undefined) {
+    classes += props.className;
+  }
+  console.log(props.children);
+  return <div className={classes}>{props.children}</div>;
 }
 export default Card;
